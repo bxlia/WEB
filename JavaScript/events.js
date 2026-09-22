90420ac5dba2678c2d0ee62dc8b9567872fe0acf
Булат <4bxlia4@gmail.com>
2026-09-22 18:56:37 +0500

    Lesson 8 - Background transition

--- JavaScript/events.js
+++ JavaScript/events.js
@@ -37,3 +37,9 @@ document.getElementById("switch-background").addEventListener("click", switchBac
 function switchBackground(e) {
 	document.body.className = document.body.className === 'dark' ? 'light' : 'dark';
 }
+document.getElementById("switch-background-delay").addEventListener("change", setDelay);
+function setDelay(e) {
+	let delay = e.target.value;
+	document.body.style.transition = document.getElementById("switch-background").transition =
+		`color ${delay}s, background-color ${delay}s, background-image ${delay}s`;
+}
